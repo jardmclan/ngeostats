2badeadf673bdc3e3cd8aca4146efad4d2295134
princessbobsanta <princessbobsantaclaus@gmail.com>
2021-02-10 15:57:01 -1000

    table combiner

---
+++ clean/table_combiner/db_connect.py
@@ -0,0 +1,231 @@
+
+
+import sqlalchemy
+import json
+from time import sleep
+from sqlalchemy import exc
+import random
+from threading import Lock, Event
+
+SSHTunnelForwarder = None
+
+class DBConnectorError(Exception):
+    pass
+
+class DBConnector():
+    def __init__(self, config = None):
+        self.tunnel = None
+        self.disposed = False
+        self.exec_lock= Lock()
+        self.exec_count_lock = Lock()
+        self.exec_count = 0
+        self.idle = Event()
+        self.idle.set()
+        #if the flag is not set then restarting
+        self.started = Event()
+        default_config_file = "config.json"
+
+        if config is None:
+            with open(default_config_file) as f:
+                config = json.load(f)["extern_db_config"]
+
+        self.config = config
+
+        self.__start()
+        self.started.set()
+        
+
+    def __start(self):
+        if self.config["tunnel"]["use_tunnel"]:
+            #only import if tunnel is None (no tunnel created before), note don't need to import this if tunneling not being used
+            if self.tunnel is None:
+                global SSHTunnelForwarder
+                from sshtunnel import SSHTunnelForwarder
+            self.__start_tunnel()
+            
+        self.__start_engine()
+
+    def __start_tunnel(self):
+        config = self.config
+        tunnel_config = config["tunnel"]["tunnel_config"]
+        self.tunnel = SSHTunnelForwarder(
+            (tunnel_config["ssh"], tunnel_config["ssh_port"]),
+            ssh_username = tunnel_config["user"],
+            ssh_password = tunnel_config["password"],
+            remote_bind_address = (tunnel_config["remote"], int(tunnel_config["remote_port"])),
+            local_bind_address = (tunnel_config["local"], int(tunnel_config["local_port"])) if tunnel_config["local_port"] is not None else (tunnel_config["local"], )
+        )
+        #looks like this should solve hanging issue on stop
+        self.tunnel.daemon_forward_servers = True
+        self.tunnel.start()
+
+    def __start_engine(self):
+        config = self.config
+        #create and populate sql configuration from config file
+        sql_config = {}
+        sql_config["lang"] = config["lang"]
+        sql_config["connector"] = config["connector"]
+        sql_config["password"] = config["password"]
+        sql_config["db_name"] = config["db_name"]
+        sql_config["user"] = config["user"]
+        sql_config["port"] = config["port"] if self.tunnel is None else self.tunnel.local_bind_port
+        sql_config["address"] = config["address"] if self.tunnel is None else self.tunnel.local_bind_host
+
+        SQLALCHEMY_DATABASE_URI = '%s+%s://%s:%s@%s:%s/%s' % (sql_config["lang"], sql_config["connector"], sql_config["user"], sql_config["password"], sql_config["address"], sql_config["port"], sql_config["db_name"])
+
+        #create engine from URI
+        self.engine = sqlalchemy.create_engine(SQLALCHEMY_DATABASE_URI)
+    
+
+    def __restart_con(self):
+        #don't want to restart multiple times
+        if self.started.is_set():
+            #clear the started flag until done restarting
+            self.started.clear()
+            def f():
+                self.__dispose_engine()
+                self.__start_engine()
+                #restarting tunnel seems to cause issues sometimes
+                # self.__cleanup_db_con()
+                # self.__start()
+            self.__block_exec_and_wait_idle(f)
+            #done restarting, set started flag
+            self.started.set()
+        #if already restarting wait on restarted flag to be set
+        else:
+            self.started.wait()
+
+    def __dispose_engine(self):
+        try:
+            self.engine.dispose()
+        except:
+            pass
+
+
+    def __dispose_tunnel(self):
+        if self.tunnel is not None:
+            try:
+                self.tunnel.stop()
+            except:
+                pass
+    
+
+    def __cleanup_db_con(self):
+        if not self.disposed:
+            self.__dispose_engine()
+            self.__dispose_tunnel()
+
+    def dispose(self):
+        self.__cleanup_db_con()
+        self.disposed = True
+
+
+
+    # def get_engine(self):
+    #     return self.engine
+
+    def __block_exec_and_wait_idle(self, f):
+        #stops new executions
+        with self.exec_lock:
+            #wait until idle
+            self.idle.wait()
+            #execute function that requires idle
+            f()
+
+    def __begin_exec(self):
+        #acquire exec lock (block if waiting on restart)
+        with self.exec_lock:
+            #stop counter races
+            with self.exec_count_lock:
+                #connection not idle
+                self.idle.clear()
+                #add exec to counter
+                self.exec_count += 1
+
+    def __end_exec(self):
+        #stop counter races
+        with self.exec_count_lock:
+            #remove exec from counter
+            self.exec_count -= 1
+            #if exec count 0 set idle
+            if self.exec_count < 1:
+                self.idle.set()
+
+
+    def __engine_exec_r(self, query, params, retries, delay = 0, last_error = None):
+        if retries < 0:
+            raise DBConnectorError("Retry limit exceeded. Last error: %s" % last_error)
+        
+        def get_backoff():
+            backoff = 0
+            #if first deadlock backoff of 0.15-0.3 seconds
+            if delay == 0:
+                backoff = 0.15 + random.uniform(0, 0.15)
+            #otherwise 2-3x current backoff
+            else:
+                backoff = delay * 2 + random.uniform(0, delay)
+            return backoff
+        def retry_query(restart):
+            backoff = 0
+            if restart:
+                #restart the connection
+                self.__restart_con()
+                #restart will wait until connection restarted so shouldn't need backoff, leave as 0
+            else:
+                #get next backoff step
+                backoff = get_backoff()
+            return self.__engine_exec_r(query, params, retries - 1, backoff, last_error)
+
+
+        #signal execution start
+        self.__begin_exec()
+        #with self.exec_lock:
+        sleep(delay)
+        res = None
+
+        restart = False
+        retry = False
+
+        #engine.begin() block has error handling logic, so try catch should be outside of this block
+        #note caller should handle errors and cleanup engine as necessary (or use with)
+        try:
+            with self.engine.begin() as con:
+                res = con.execute(query, params) if params is not None else con.execute(query)
+        except exc.OperationalError as e:
+            last_error = e
+            retry = True
+            #if not deadlock error (code 1213) also restart connection
+            if e.orig.args[0] != 1213:
+                restart = True
+        except Exception as e:
+            last_error = e
+            #something went wrong, restart connection and retry
+            restart = True
+            retry = True
+        
+        #indicate end of execution
+        self.__end_exec()
+        #retry if indicated
+        if retry:
+            #retry query and specify whether to restart connection
+            res = retry_query(restart)
+        
+        #return query result
+        return res
+
+
+    def engine_exec(self, query, params, retry):
+        if self.disposed:
+            raise RuntimeError("Cannot call engine exec after cleanup_db_engine. Object disposed")
+        res = self.__engine_exec_r(query, params, retry)
+        return res
+
+    
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.dispose()
+        if exc_type is not None:
+            raise DBConnectorError("An error occured while using database connector: type: %s, error: %s" % (exc_type.__name__, exc_val))
---
+++ clean/table_combiner/driver.py
@@ -0,0 +1,166 @@
+import math
+from db_connect import DBConnector, DBConnectorError
+from sys import argv, stderr
+from json import load
+
+##########################################
+
+import mpi4py
+mpi4py.rc.recv_mprobe = False
+
+from mpi4py import MPI
+
+comm = MPI.COMM_WORLD
+
+
+##########################################
+
+distributor_rank = 0
+
+#load config
+if len(argv) < 2:
+    raise RuntimeError("Invalid command line args. Must provide config file")
+config_file = argv[1]
+config = None
+with open(config_file) as f:
+    config = load(f)
+
+
+##########################################
+
+#process rank
+rank = comm.Get_rank()
+size = comm.Get_size()
+processor_name = MPI.Get_processor_name()
+
+##########################################
+
+#combine table t2 into t1
+def combine_tables(t1, t2):
+    #tester
+    print("t1: %s, t2: %s (combine to %s)" % (t1, t2))
+    # global connector
+    # global config
+    # retry = config["general"]["db_retry"]
+    # query = """
+    #     INSERT IGNORE INTO %s
+    #     SELECT *
+    #     FROM %s
+    # """ % (t1, t2)
+    # connector.engine_exec(query, None, retry)
+    return t1
+
+
+def partition_tables(tables):
+    tables_range = len(tables)
+    tables_pivot = math.ceil(tables_range / 2.0)
+    tables_lower = tables[:tables_pivot]
+    tables_upper = tables[tables_pivot:]
+    return (tables_lower, tables_upper)
+
+def partition(ranks, tables):
+    ranks_range = ranks[1] - ranks[0]
+    ranks_pivot = math.ceil(ranks_range / 2.0)
+    ranks_lower = [ranks[0], ranks_pivot]
+    ranks_upper = [ranks_pivot, ranks[1]]
+
+    table_parts = partition_tables(tables)
+    tables_lower = table_parts[0]
+    tables_upper = table_parts[1]
+
+    return [(ranks_lower, tables_lower), (ranks_upper, tables_upper)]
+
+
+def handle_tables_local(tables):
+    if len(tables < 1):
+        raise ValueError("Tables list has no items. This should never happen unless the initial list is empty.")
+    #only one table, reached bottom of recursion, return single table
+    if len(tables) == 1:
+        return tables[0]
+    
+    parts = partition_tables(tables)
+
+    t1 = handle_tables_local(parts[0][1])
+    t2 = handle_tables_local(parts[1][1])
+
+
+    combined = combine_tables(t1, t2)
+    return combined
+
+
+
+def handle_tables(data):
+    ranks = data[0]
+    tables = data[1]
+
+    if len(tables < 1):
+        raise ValueError("Tables list has no items. This should never happen unless the initial list is empty.")
+    #only one table, reached bottom of recursion, return single table
+    if len(tables) == 1:
+        return tables[0]
+    
+    parts = partition(ranks, tables)
+
+    t1 = None
+    t2 = None
+    
+    #distribute first group
+    dist_ranks = parts[0][0]
+    dist_tables = parts[0][1]
+    #handle second group locally
+    local_data = parts[1]
+
+    dist_rank_range = dist_ranks[1] - dist_ranks[0]
+    #if no ranks to distribute to then handle the rest locally
+    if dist_rank_range == 0:
+        #note using ceil so first group (dist group) always will have equal or greater the number of ranks, so if first group 0, second group wont have any either (can use local for both)
+        t1 = handle_tables_local(parts[0][1])
+        t2 = handle_tables_local(parts[1][1])
+    else:
+        #first rank in set of distributor ranks next rank to send to
+        dist_rank = dist_ranks[0]
+        #remaining ranks
+        dist_rank_sub = dist_ranks[1:]
+        #data to distribute (parent (current rank), ranks, tables)
+        dist_data = (rank, dist_rank_sub, dist_tables)
+        #send off first group to be processed by the next rank
+        comm.send(dist_data, dest = dist_rank)
+        #recursively handle second group
+        
+        t1 = handle_tables(dist_data)
+        #get the table sent off to the next rank
+        t2 = comm.recv()
+
+
+    combined = combine_tables(t1, t2)
+    return combined
+
+
+
+
+def get_tables():
+    data = comm.recv()
+    parent = data[0]
+    ranks = data[1]
+    tables = data[2]
+
+    combined = handle_tables((ranks, tables))
+
+    comm.send(combined, dest = parent)
+
+    print("Rank %s finished (tables: %s)" % (rank, tables))
+        
+
+
+db_config = config["extern_db_config"]
+with DBConnector(db_config) as connector:
+    if rank == distributor_rank:
+        print("Starting root, rank: %d, node: %s" % (rank, processor_name))
+        ranks = [0, size]
+        tables = []
+        root_table = handle_tables((ranks, tables))
+        print("Complete! Root table %s" % root_table)
+        
+    else:
+        print("Starting rank: %d, node: %s" % (rank, processor_name))
+        get_tables()
